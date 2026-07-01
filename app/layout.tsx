9a81c323353b3040f33117323dab5825d50f135f
NoName <kirillur424@gmail.com>
2026-07-01 15:04:50 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Analytics } from '@vercel/analytics/next'
 import type { Metadata, Viewport } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] })
 const geistMono = Geist_Mono({
@@ -49,6 +50,19 @@ export default function RootLayout({
   return (
     <html lang="en" className={`dark ${geistSans.variable} ${geistMono.variable} bg-background`}>
       <body className="font-sans antialiased bg-background text-foreground">
+        <Script id="theme-init" strategy="beforeInteractive">
+  {`
+    try {
+      const theme = localStorage.getItem('theme');
+
+      if (theme === 'green') {
+        document.documentElement.setAttribute('data-theme', 'green');
+      } else {
+        document.documentElement.removeAttribute('data-theme');
+      }
+    } catch {}
+  `}
+</Script>
         {children}
         {process.env.NODE_ENV === 'production' && <Analytics />}
       </body>
